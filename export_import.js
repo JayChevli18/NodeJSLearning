fcd853a48200767ba5680e4d1ab24fedf9579ed3
JayChevli18 <chevlijay70@gmail.com>
2024-09-11 19:13:52 +0530

    Export_Import

---
+++ export_import.js
@@ -0,0 +1,21 @@
+const add=(a,b)=>{
+    return a+b;
+}
+
+const sub=(a,b)=>{
+    return a-b;
+}
+
+module.exports.mul=(a,b)=>{
+    return a*b;
+}
+
+//Not preferrable
+exports.div=(a,b)=>{
+    return a/b;
+}
+
+// module.exports={
+//     add,
+//     sub
+// }
---
+++ index_exp_imp.js
@@ -0,0 +1,10 @@
+//Export - Import
+
+const math=require("./export_import");
+
+// const {add, sub}=math;
+
+// console.log(add(2,3));
+// console.log(sub(5,1));
+console.log(math.mul(4,2));
+console.log(math.div(8,2.3));
