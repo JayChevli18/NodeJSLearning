1a99ccf7d7bfd39e3952021432ee08a41476e41d
JayChevli18 <chevlijay70@gmail.com>
2024-09-11 22:03:43 +0530

    math-esm-2nd way

---
+++ main.mjs
@@ -0,0 +1,15 @@
+//1st way
+// import math from "./math-esm.mjs";
+
+// const {add, sub}=math;
+
+// console.log(add(2,3));
+// console.log(sub(3,3));
+
+
+//2nd way
+
+import {add, sub} from "./math-esm.mjs";
+
+console.log(add(5,7));
+console.log(sub(5,3));
---
+++ math-esm.mjs
@@ -0,0 +1,27 @@
+//1st way
+// const add=(a,b)=>{
+//     return a+b;
+// }
+
+// const sub=(a,b)=>{
+//     return a-b;
+// }
+
+
+// export default{
+//     add,
+//     sub
+// };
+
+
+//2nd way
+
+
+export const add=(a,b)=>{
+    return a+b;
+}
+
+export const sub=(a,b)=>{
+    return a-b;
+}
+
