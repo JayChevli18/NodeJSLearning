694178fe4c8b5964771a502dc6f77b34fae27a03
JayChevli18 <chevlijay70@gmail.com>
2024-09-19 21:02:28 +0530

    class_event

---
+++ pizza-shop.js
@@ -0,0 +1,12 @@
+class PizzaShop{
+    constructor(){
+        this.orderNumber=0;
+    }
+    order(){
+        this.orderNumber++;
+    }
+
+    displayOrder(){
+        console.log(`Current Order Number:${this.orderNumber} `);
+    }
+}
