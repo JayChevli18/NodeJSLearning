fdbd40f06e6d8be2dc7164e095bc58bfba18f1f2
JayChevli18 <chevlijay70@gmail.com>
2024-09-29 22:48:15 +0530

    Working with JSON Data-HTML-Normal Routes

---
+++ NodeJS_Basic/app.js
@@ -0,0 +1,70 @@
+const readline=require('readline');
+const fs=require('fs');
+const http=require('http');
+const url=require('url');
+const events=require('events');
+
+const html=fs.readFileSync('./Template/index.html','utf-8');
+let products=JSON.parse(fs.readFileSync('./Data/products.json', 'utf-8'));
+let productListHtml=fs.readFileSync('./Template/product-list.html', 'utf-8');
+
+let productHtmlArray=products.map((prod)=>{
+    let output=productListHtml.replace('{{%IMAGE%}}', prod.productImage);
+    output=output.replace('{{%NAME%}}', prod.name);
+    output=output.replace('{{%MODELNAME%}}', prod.modeName);
+    output=output.replace('{{%MODELNO%}}', prod.modelNumber);
+    output=output.replace('{{%SIZE%}}', prod.size);
+    output=output.replace('{{%CAMERA%}}', prod.camera);
+    output=output.replace('{{%PRICE%}}', prod.price);
+    output=output.replace('{{%COLOR%}}', prod.color);
+
+    return output;
+})
+
+const server=http.createServer((req, res)=>{
+    let path=req.url;
+
+    if(path==='/' || path.toLocaleLowerCase()==='/home')
+    {
+        res.writeHead(200, {
+            'Content-Type':'text/html',
+            'my-header':'Hello world'
+        });
+        res.end(html.replace('{{%CONTENT%}}', productListHtml));
+    }
+    else if(path.toLocaleLowerCase()==='/about'){
+        res.writeHead(200, {
+            'Content-Type':'text/html',
+            'my-header':'Hello world'
+        });
+        res.end(html.replace('{{%CONTENT%}}', 'You are in ABOUT page'));
+    }
+    else if(path.toLocaleLowerCase()==='/contact'){
+        res.writeHead(200, {
+            'Content-Type':'text/html',
+            'my-header':'Hello world'
+        });
+        res.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'));
+    }
+    else if(path.toLocaleLowerCase()==='/products'){
+        let productResponseHtml=html.replace('{{%CONTENT%}}', productHtmlArray.join(','));
+        res.writeHead(200, {
+            'Content-type':'text/html'
+        })
+        res.end(productResponseHtml);
+    }
+    else{
+        res.writeHead(404,{
+            'Content-Type':'text/html',
+            'my-header':'Hello world!'
+        })
+        res.end(html.replace('{{%CONTENT%}}', 'Error 404: Page Not Found'))
+    }
+
+})
+
+
+
+server.listen(4000, ()=>{
+    console.log("Server Started at port 4000");
+})
